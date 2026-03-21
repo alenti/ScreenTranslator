88051130ef61ae87a09f85f32db74d97c6ae5711
alenti <shamatovalen@gmail.com>
2026-03-21 19:23:01 +0800

    Initial Commit

---
+++ ScreenTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTranslator
+//
+//  Created by Notnik_kg on 21.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTranslator/ScreenTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTranslatorApp.swift
+//  ScreenTranslator
+//
+//  Created by Notnik_kg on 21.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
